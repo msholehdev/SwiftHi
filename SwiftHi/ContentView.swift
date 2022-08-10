a32a6863e123a65e283691915f2bc0e7dcf803b1
Muhamad Sholeh <87067730+msholehdev@users.noreply.github.com>
2022-08-10 11:11:29 +0700

    first commit
    first commit

---
+++ SwiftHi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftHi
+//
+//  Created by muhamad sholeh on 10/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftHi/SwiftHiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftHiApp.swift
+//  SwiftHi
+//
+//  Created by muhamad sholeh on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftHiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
